05d16e5592d1876ab4efd320bfd89974937df2d0
Giulliano Massarelli <giuliano.massarelli@geradordedevs.com.br>
2022-12-05 18:30:49 -0300

    :tada: Commit inicial

---
+++ src/main/java/br/com/gd/pagarme/PagarMeApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gd.pagarme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagarMeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagarMeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gd/pagarme/PagarMeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gd.pagarme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagarMeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
