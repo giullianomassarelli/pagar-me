c09d5c1d8f21f9d754bf6b81383c50154e4f8c8e
Giulliano Massarelli <giuliano.massarelli@geradordedevs.com.br>
2022-12-06 11:38:51 -0300

    :recycle: refactor: Trocando para o MongoRepository

--- src/main/java/br/com/gd/pagarme/repositories/PayableRepository.java
+++ src/main/java/br/com/gd/pagarme/repositories/PayableRepository.java
@@ -1,7 +1,8 @@
 package br.com.gd.pagarme.repositories;
 
 import br.com.gd.pagarme.entities.PayableEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PayableRepository extends CrudRepository<PayableEntity, String> {
+public interface PayableRepository extends MongoRepository<PayableEntity, String> {
 }
--- src/main/java/br/com/gd/pagarme/repositories/TransacaoRepository.java
+++ src/main/java/br/com/gd/pagarme/repositories/TransacaoRepository.java
@@ -1,7 +1,8 @@
 package br.com.gd.pagarme.repositories;
 
 import br.com.gd.pagarme.entities.TransacaoEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TransacaoRepository extends CrudRepository<TransacaoEntity, String> {
+public interface TransacaoRepository extends MongoRepository<TransacaoEntity, String> {
 }
