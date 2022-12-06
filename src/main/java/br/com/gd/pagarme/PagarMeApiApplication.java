73247116aa2fb808075e78f0c42dd9a8598ac62b
Giulliano Massarelli <giuliano.massarelli@geradordedevs.com.br>
2022-12-06 11:52:48 -0300

    :heavy_plus_sign: build: Adcionando dependencia do model mapper

--- src/main/java/br/com/gd/pagarme/PagarMeApiApplication.java
+++ src/main/java/br/com/gd/pagarme/PagarMeApiApplication.java
@@ -1,13 +1,20 @@
 package br.com.gd.pagarme;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PagarMeApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PagarMeApiApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new org.modelmapper.ModelMapper();
 	}
 
 }
