0516ed891ecbcf7f0611ec4f8c3c446d9f4b19c5
Giulliano Massarelli <giuliano.massarelli@geradordedevs.com.br>
2022-12-06 11:49:18 -0300

    :sparkles: feat: Criando camada de Serviços para Pagamentos

---
+++ src/main/java/br/com/gd/pagarme/services/impl/PagamentoServiceImpl.java
@@ -0,0 +1,28 @@
+package br.com.gd.pagarme.services.impl;
+
+import br.com.gd.pagarme.entities.PagamentoEntity;
+import br.com.gd.pagarme.repositories.PagamentoRepository;
+import br.com.gd.pagarme.services.PagamentoService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class PagamentoServiceImpl implements PagamentoService {
+
+    @Autowired
+    private PagamentoRepository pagamentoRepository;
+
+    @Override
+    public PagamentoEntity salvar(PagamentoEntity pagamentoEntity) {
+        log.info("Salvando um novo pagamento");
+        return pagamentoRepository.save(pagamentoEntity);
+    }
+
+    @Override
+    public void deletar() {
+        log.warn("Deletando todos os pagamentos do banco de dados");
+        pagamentoRepository.deleteAll();
+    }
+}
