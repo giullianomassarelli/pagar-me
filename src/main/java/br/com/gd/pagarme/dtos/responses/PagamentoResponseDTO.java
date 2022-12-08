71d1544ced78c633642daeea46aaa99c484887c5
Giulliano Massarelli <giuliano.massarelli@geradordedevs.com.br>
2022-12-08 10:16:48 -0300

    :zap: perf: Melhorando as respostas da API

--- src/main/java/br/com/gd/pagarme/dtos/responses/PagamentoResponseDTO.java
+++ src/main/java/br/com/gd/pagarme/dtos/responses/PagamentoResponseDTO.java
@@ -2,6 +2,7 @@ package br.com.gd.pagarme.dtos.responses;
 
 import br.com.gd.pagarme.enums.MetodoPagamentoEnum;
 import br.com.gd.pagarme.enums.PagamentoEnum;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -9,7 +10,8 @@ import java.time.LocalDateTime;
 
 @Data
 public class PagamentoResponseDTO {
-
+   @JsonProperty("Status")
    private PagamentoEnum status;
+   @JsonProperty("Data do pagamento")
    private LocalDateTime dataPagamento;
 }
--- src/main/java/br/com/gd/pagarme/dtos/responses/TransacaoResponseDTO.java
+++ src/main/java/br/com/gd/pagarme/dtos/responses/TransacaoResponseDTO.java
@@ -1,6 +1,7 @@
 package br.com.gd.pagarme.dtos.responses;
 
 import br.com.gd.pagarme.enums.MetodoPagamentoEnum;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -9,12 +10,27 @@ import java.util.List;
 @Data
 public class TransacaoResponseDTO {
 
+   @JsonProperty("Descrição")
    private String descricao;
+
+   @JsonProperty("Valor da transação")
    private BigDecimal valorTransacao;
+
+   @JsonProperty("Metodo de pagamento")
    private MetodoPagamentoEnum metodoPagamento;
+
+   @JsonProperty("Numero do cartão")
    private String numeroCartao;
+
+   @JsonProperty("Portador do cartão")
    private String nomePortadorCartao;
+
+   @JsonProperty("Data de validade do cartão")
    private String dataValidadeCartao;
+
+   @JsonProperty("CVV")
    private String cvv;
+
+   @JsonProperty("Informações do pagamento")
    private PagamentoResponseDTO pagamento;
 }
