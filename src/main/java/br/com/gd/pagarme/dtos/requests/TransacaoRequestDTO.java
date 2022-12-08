a3166bd3a5ddd252378647b41b3b1098f7b430d7
Giulliano Massarelli <giuliano.massarelli@geradordedevs.com.br>
2022-12-08 10:17:34 -0300

    :sparkles: feat: Criando validações para a TransacaoRequestDTO

--- src/main/java/br/com/gd/pagarme/dtos/requests/TransacaoRequestDTO.java
+++ src/main/java/br/com/gd/pagarme/dtos/requests/TransacaoRequestDTO.java
@@ -4,16 +4,34 @@ import br.com.gd.pagarme.dtos.responses.PagamentoResponseDTO;
 import br.com.gd.pagarme.enums.MetodoPagamentoEnum;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 
 @Data
 public class TransacaoRequestDTO {
+
+    @NotNull
     private String descricao;
+
+    @NotNull
     private BigDecimal valorTransacao;
+
+    @NotNull
     private MetodoPagamentoEnum metodoPagamento;
+
+    @Size(min = 16, max = 16, message = "Informe todos os numeros do cartão")
     private String numeroCartao;
+
+    @NotNull
     private String nomePortadorCartao;
+
+    @NotNull
     private String dataValidadeCartao;
+
+    @NotNull
+    @Size(min = 3, max = 3, message = "Verifique o código de segurança do cartão")
     private String cvv;
+
     private PagamentoResponseDTO pagamento;
 }
