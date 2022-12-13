b7a233855bf199a7cbe88e2e370a7ca645ce2431
Giulliano Massarelli <giuliano.massarelli@geradordedevs.com.br>
2022-12-13 14:58:13 -0300

    :zap: perf: melhorando a transacao facade impl e removendo os conversores, e injetando a classe TransacaoMapper

--- src/main/java/br/com/gd/pagarme/facades/impl/TransacaoFacadeImpl.java
+++ src/main/java/br/com/gd/pagarme/facades/impl/TransacaoFacadeImpl.java
@@ -6,8 +6,9 @@ import br.com.gd.pagarme.dtos.responses.TransacaoResponseDTO;
 import br.com.gd.pagarme.entities.TransacaoEntity;
 import br.com.gd.pagarme.enums.MetodoPagamentoEnum;
 import br.com.gd.pagarme.enums.PagamentoEnum;
-import br.com.gd.pagarme.facades.PagamentoFacade;
 import br.com.gd.pagarme.facades.TransacaoFacade;
+import br.com.gd.pagarme.mappers.PagamentoMapper;
+import br.com.gd.pagarme.mappers.TransacaoMapper;
 import br.com.gd.pagarme.services.TransacaoService;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -21,54 +22,50 @@ import java.util.List;
 @Component
 @Slf4j
 public class TransacaoFacadeImpl implements TransacaoFacade {
+
     @Autowired
     private TransacaoService transacaoService;
+
     @Autowired
-    private PagamentoFacade pagamentoFacade;
-    @Autowired
-    private ModelMapper modelMapper;
-    private final BigDecimal TAXA_DEBITO = new BigDecimal("0.03");
-    private final BigDecimal TAXA_CREDITO = new BigDecimal("0.05");
+    private TransacaoMapper transacaoMapper;
+
     @Override
     public TransacaoResponseDTO salvar(TransacaoRequestDTO transacaoRequestDTO) {
+        transacaoRequestDTO.setNumeroCartao(transacaoMapper.esconderNumerosDoCartao(transacaoRequestDTO.getNumeroCartao()));
 
-        transacaoRequestDTO.setNumeroCartao(esconderNumerosDoCartao(transacaoRequestDTO.getNumeroCartao()));
-
-        transacaoRequestDTO.setPagamento(pagamentoFacade.criarPagamento(transacaoRequestDTO.getMetodoPagamento()));
-
-        return converterTransacaoEntityParaTransacaoResponseDTO(
-                transacaoService.salvar(converterTransacaoRequestDTOParaTransacaoEntity(transacaoRequestDTO)));
+        return transacaoMapper.converterTransacaoEntityParaTransacaoResponseDTO(
+                transacaoService.salvar(transacaoMapper.converterTransacaoRequestDTOParaTransacaoEntity(transacaoRequestDTO)));
     }
+
     @Override
     public List<TransacaoResponseDTO> listar() {
-
         List<TransacaoResponseDTO> transacaoResponseDTOList = new ArrayList<>();
+
         for (TransacaoEntity transacaoEntity : transacaoService.listar()) {
-            transacaoResponseDTOList.add(converterTransacaoEntityParaTransacaoResponseDTO(transacaoEntity));
+            transacaoResponseDTOList.add(transacaoMapper.converterTransacaoEntityParaTransacaoResponseDTO(transacaoEntity));
         }
 
         return transacaoResponseDTOList;
     }
+
     @Override
     public SaldoResponseDTO consultarSaldo() {
+        log.info("consultando saldo disponivel");
 
         BigDecimal saldoDisponivel = BigDecimal.ZERO;
         BigDecimal saldoALiberar = BigDecimal.ZERO;
-        BigDecimal taxa;
-        BigDecimal saldoAtualizado;
+        BigDecimal saldoTotal;
 
-        log.info("Consultando saldo disponivel");
         for (TransacaoEntity transacaoEntity : transacaoService.listar()){
-            BigDecimal valorTransacao = transacaoEntity.getValorTransacao();
-                if (transacaoEntity.getPagamento().getStatus() == PagamentoEnum.PAID){
-                    taxa = valorTransacao.multiply(TAXA_DEBITO);
-                    saldoAtualizado = valorTransacao.subtract(taxa);
-                    saldoDisponivel = saldoDisponivel.add(saldoAtualizado);
-                } else {
-                    taxa = valorTransacao.multiply(TAXA_CREDITO);
-                    saldoAtualizado = valorTransacao.subtract(taxa);
-                    saldoALiberar = saldoALiberar.add(saldoAtualizado);
-                }
+            log.info("transacao de id: {}, valor : {}", transacaoEntity.getId(), transacaoEntity.getValorTransacao());
+
+            if (transacaoEntity.getMetodoPagamento() == MetodoPagamentoEnum.DEBIT_CARD){
+                saldoDisponivel = saldoDisponivel.add(transacaoMapper.calculoTaxaDebito(transacaoEntity.getValorTransacao()));
+            } else {
+                saldoALiberar = saldoALiberar.add(transacaoMapper.calculoTaxaCredito(transacaoEntity.getValorTransacao()));
+            }
+
+            saldoTotal = saldoDisponivel.add(saldoALiberar);
         }
 
         SaldoResponseDTO saldoResponseDTO = new SaldoResponseDTO();
@@ -84,16 +81,4 @@ public class TransacaoFacadeImpl implements TransacaoFacade {
         transacaoService.deletar();
     }
 
-    private String esconderNumerosDoCartao (String string){
-        String NUM_CARTAO_CRIP = "XXXX-XXXX-XXXX-";
-        return NUM_CARTAO_CRIP + string.substring(string.length() -4);
-    }
-
-    private TransacaoResponseDTO converterTransacaoEntityParaTransacaoResponseDTO(TransacaoEntity transacaoEntity) {
-        return modelMapper.map(transacaoEntity, TransacaoResponseDTO.class);
-    }
-
-    private TransacaoEntity converterTransacaoRequestDTOParaTransacaoEntity(TransacaoRequestDTO transacaoRequestDTO) {
-        return modelMapper.map(transacaoRequestDTO, TransacaoEntity.class);
-    }
 }
